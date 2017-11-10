909d8ae9ba32c7449f69d7bfef7044186f246038
qiubei <qiubei@entertech.cn>
2017-11-10 12:09:30 +0800

    create travis ci ios demo

---
+++ TravisCIDemoiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisCIDemoiOS
+//
+//  Created by Anonymous on 2017/11/10.
+//  Copyright © 2017年 Hangzhou Enter Electronic Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
